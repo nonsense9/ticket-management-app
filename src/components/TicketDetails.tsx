70b0d46eff3e4430afe6750c4b9a25d087f81881
Alexey Ceaicovschiy <alexeyc3991@gmail.com>
2023-06-13 21:12:02 +0300

    add req to get ticket from service

--- src/components/TicketDetails.tsx
+++ src/components/TicketDetails.tsx
@@ -1,13 +1,25 @@
-import {useLocation, useNavigate} from "react-router";
+import {useNavigate, useParams} from "react-router";
 import {Status} from "./Status";
+import {ApiService, Ticket} from "../api";
+import {useEffect, useState} from "react";
+const apiService = new ApiService();
 
 export const TicketDetails = () => {
-    const {state} = useLocation();
+    const params = useParams();
     const navigate = useNavigate();
+    const [ticket, setTicket] = useState({} as Ticket)
+
+    useEffect(() => {
+        apiService.ticket(Number(params.id)).subscribe((res) => setTicket(res))
+    }, [params?.id])
     return <div>
         <div><button onClick={() => navigate(-1)}>Back</button></div>
-        <div><h2>Ticket ID: {state.id}</h2></div>
-        <div><p>Description: {state.description}</p></div>
-        <div><p>Status: <Status status={state.status}/></p></div>
+        <div><h2>Ticket ID: {ticket.id}</h2></div>
+        <div><p>Description: {ticket.description}</p></div>
+        <div><p>Status: <Status status={ticket.completed}/></p></div>
+        <div><button onClick={() => {
+            apiService.complete(ticket.id)
+            apiService.ticket(Number(params.id)).subscribe((res) => setTicket(res))
+        }}>Complete</button></div>
     </div>
 }
