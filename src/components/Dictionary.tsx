d8b645269d9fa055c05bae48f48f5d006e8c68c6
Alexey Ceaicovschiy <alexeyc3991@gmail.com>
2023-06-20 00:13:14 +0300

    handle 404 error and show source info

--- src/components/Dictionary.tsx
+++ src/components/Dictionary.tsx
@@ -1,18 +1,19 @@
 import React, {useState} from "react";
 
 export const Dictionary = () => {
+    const dictionaryUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
     const [dictionary, setDictionary] = useState([{}] as any);
     const [word, setWord] = useState('' as string);
 
     const getDictionary = () => {
         if (!word) return;
-        return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, {
+        return fetch(`${dictionaryUrl}${word}`, {
             method: 'GET',
-        }).then((res) => res.json()).then(res => setDictionary(res))
+        }).then((res) => res.json().then(res => setDictionary(res)));
     }
     const getAudio = () => {
-        const audio = dictionary.map((i: {phonetics: any;}) => i.phonetics[0])
-        let a = new Audio(audio[0].audio)
+        const audio = dictionary.map((i: {phonetics: any;}) => i.phonetics[0].audio ? i.phonetics[0] : i.phonetics[1])
+        let a = new Audio(audio[0].audio);
         a.play();
     }
 
@@ -23,6 +24,9 @@ export const Dictionary = () => {
             <button onClick={getDictionary}>Get Audio</button>
             <button onClick={getAudio}>Play Audio</button>
         </div>
-        {dictionary ? JSON.stringify(dictionary) : null}
+        {dictionary && dictionary.title ?
+            <div style={{color: 'red'}}>{dictionary.message}</div> : dictionary.map((item: any, idx: number) => {
+                return <div key={idx}>{item.sourceUrls}</div>
+            })}
     </div>
 }
