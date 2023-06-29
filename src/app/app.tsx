cda92f909f4d08bbb09e7e19beae2d3fa0ec1ed8
Alexey Ceaicovschiy <alexeyc3991@gmail.com>
2023-06-29 13:55:06 +0300

    add redirect from default path & show current location for map on first open

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./app.scss";
 import {Tickets} from "../components/Tickets";
-import {RouterProvider} from "react-router";
+import {Navigate, RouterProvider} from "react-router";
 import {createBrowserRouter} from "react-router-dom";
 import {TicketDetails} from "../components/TicketDetails";
 import {ApiService} from "../api/service";
 import {Layout} from "../components/Layout";
 import {Dictionary} from "../components/Dictionary";
 import {Map} from "../components/Map";
-import { Wrapper } from "@googlemaps/react-wrapper";
+import {Wrapper} from "@googlemaps/react-wrapper";
+
 const apiService = new ApiService();
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout/>,
+        element: <Navigate to="tickets"/>,
+        index: true,
+    },
+    {
+        path: '/',
+        element: <><Layout/></>,
         children: [
             {
                 path: 'tickets',
@@ -30,7 +36,7 @@ const router = createBrowserRouter([
             },
             {
                 path: 'map',
-                element: <Wrapper apiKey={process.env.REACT_APP_API_KEY as string}><Map center={{lat: 47, lng: 28}} zoom={8}/></Wrapper>
+                element: <Wrapper apiKey={process.env.REACT_APP_API_KEY as string}><Map/></Wrapper>
             }
         ]
     },
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,28 @@
-import {useEffect, useRef} from "react";
-export const Map = ({
-                        center,
-                        zoom,
-                    }: {
-    center: google.maps.LatLngLiteral;
-    zoom: number;
-}) => {
+import {useEffect, useRef, useState} from "react";
+export const Map = () => {
     const ref = useRef<any>();
+    const [center, setCenter] = useState<any>(null)
+    const zoom = 8;
+
+    const getCoords = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const {latitude, longitude} = position?.coords;
+                latitude && longitude? setCenter({lat: latitude, lng: longitude}) : setCenter(null)
+            })
+        }
+    }
+    useEffect(() => {
+        getCoords();
+    }, [])
 
     useEffect(() => {
         new window.google.maps.Map(ref.current, {
             center,
             zoom,
         });
-    });
+    }, [center]);
+
 
     return <div ref={ref} id="map"/>;
 }
